edfd4a8b407c30806802198348df9185c553d940
Mikmik28 <mikeeagregado28@gmail.com>
2022-11-24 16:13:09 +0800

    Adds typescript

--- app/javascript/index.js
+++
@@ -1,5 +0,0 @@
-// Map Components to a specific HTML Tags
-import { define } from "remount";
-import Hello from "./shared-components/Hello";
-
-define({ "c-hello": Hello });
--- app/javascript/shared-components/Hello.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ReactDOM } from "react-dom";
-
-const Hello = () => {
-  return <div>Hello, Rails 7!</div>;
-};
-console.log("RUNNING");
-
-export default Hello;
