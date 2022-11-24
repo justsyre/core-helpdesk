5c345d559b97364c584ca2f7b48d82c0d7fa2cb3
Mikmik28 <mikeeagregado28@gmail.com>
2022-11-24 18:40:13 +0800

    Adds rubocop and setups rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'hello/hello'
-  get 'test/hello'
+  get "hello/hello"
+  get "test/hello"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
